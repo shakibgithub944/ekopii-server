7620adb44d43ac3f031132ca2acaa2e48aed4bc5
shakibgithub554 <shakibmiah554@gmail.com>
2023-03-03 02:34:53 +0600

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express();
@@ -29,12 +29,48 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/user/:user_type', async (req, res) => {
-      const user_type = req.params.user_type
-      const query = { user_type: user_type }
+    app.get('/user/:userType', async (req, res) => {
+      const user_type = req.params.userType
+      const query = { userType: user_type }
       const result = await UserCollections.find(query).toArray();
       res.send(result);
     })
+
+    app.post('/addUser', async (req, res) => {
+      const user = req.body;
+      const result = await UserCollections.insertOne(user);
+      res.send(result);
+    })
+
+    app.get('/singleUser/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await UserCollections.findOne(query);
+      res.send(result)
+    })
+
+    app.put('/updatedUser/:id', async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      const firstName = user.firstName
+      const lastName = user.lastName
+      const division = user.division
+      const district = user.district
+
+      const query = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updatedDoc = {
+        $set: {
+          firstName,
+          lastName,
+          division,
+          district
+        }
+      }
+      const result = await UserCollections.updateOne(query, updatedDoc, options);
+      res.send(result)
+    })
+
 
   }
   finally {
